9121320ec12f445fc914a3c98428b06e572b3148
temukuevalim2008-crypto <temukuevalim2008@gmail.com>
2026-08-13 03:30:51 +0300

    Update TemplateMod.java

--- .github/workflows/src/main/java/com/example/TemplateMod.java
+++ .github/workflows/src/main/java/com/example/TemplateMod.java
@@ -1,17 +1,10 @@
-package net.fabricmc.example;
+package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class TemplateMod implements ModInitializer {
-    // Создаем экземпляр твоей панельки TargetHUD
-    private static final TargetHud targetHud = new TargetHud();
-
     @Override
     public void onInitialize() {
-        // Подключаем худ к экрану отрисовки Майнкрафта
-        HudRenderCallback.EVENT.register((guiGraphics, tickCounter) -> {
-            targetHud.render();
-        });
+        // Здесь ничего для худа писать не нужно, общая инициализация
     }
 }
