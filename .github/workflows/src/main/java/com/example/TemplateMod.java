855f7588ea7b62aec17d2a09864ec6156a8b140e
temukuevalim2008-crypto <temukuevalim2008@gmail.com>
2026-08-13 03:25:24 +0300

    Update TemplateMod.java

--- .github/workflows/src/main/java/com/example/TemplateMod.java
+++ .github/workflows/src/main/java/com/example/TemplateMod.java
@@ -1,30 +1,17 @@
-package com.example;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.resources.Identifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "template-mod";
+    // Создаем экземпляр твоей панельки TargetHUD
+    private static final TargetHud targetHud = new TargetHud();
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
+    @Override
+    public void onInitialize() {
+        // Подключаем худ к экрану отрисовки Майнкрафта
+        HudRenderCallback.EVENT.register((guiGraphics, tickCounter) -> {
+            targetHud.render();
+        });
+    }
 }
